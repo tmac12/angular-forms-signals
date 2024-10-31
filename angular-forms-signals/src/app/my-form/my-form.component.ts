c158b6129d1346f9606c74206dc8ee3c790b84d8
tmac12 <mba121@gmail.com>
2024-10-31 15:07:55 +0100

    chore: remove unused imports

--- angular-forms-signals/src/app/my-form/my-form.component.ts
+++ angular-forms-signals/src/app/my-form/my-form.component.ts
@@ -4,7 +4,6 @@ import { toSignal } from '@angular/core/rxjs-interop';
 import {
   FormBuilder,
   FormControl,
-  FormGroup,
   ReactiveFormsModule,
   Validators,
 } from '@angular/forms';
@@ -29,6 +28,9 @@ export class MyFormComponent {
     }
   );
 
+  // emailSignal is created using the toSignal function.
+  // represents the value of the 'email' form control.
+  // so now instead of manually subscribing to the valueChanges of the form control, we can directly use the signal in our template
   emailSignal = toSignal(
     this.profileForm.get('email')?.valueChanges.pipe(debounceTime(300)) ??
       of(null),
